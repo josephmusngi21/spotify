5a1017be4de18e32fd899f98644f765d3410b6f2
Josephmusngi21 <josephmusngi7@gmail.com>
2024-07-23 01:07:53 -0700

    Added useStates and functions for search, addTracks, removeTrack, updatePlaylist, savePlaylist

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from '../..';
-import Playlist from './Playlist';
+import React, { useState, useCallback } from "react";
+import "./App.module.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>
-          Ja<span className='highlight'>mmm</span>ing
-        </h1>
-        <div className='App'>
-          <div className='App-playlist'>
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResult/SearchResults";
+import Spotify from "../util/spotify";
+import Tracklist from "../Tracklist/Tracklist";
 
-            {/* Add a SearchBar component */}
-          </div>
-          {/* Add a SearchResults component */}
-          {/* Add a Playlist component */}
-        </div>
-      </div>
-    );
-  }
+function App() {
+
+  // make a usestate for SearchResults, playlistname and playlist Tracklist
+  const [searchResult, setSearchResult] = useState([]);
+  const [playlistName, setplaylistName] = useState("New Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  // then create a function search that will check for the search results and set it
+  const search = useCallback((term) => {
+    Spotify.search(term).then(setSearchResult);
+  }, []);
+  
+  // then create a function to add tracks using useCallback and set the playlisttracks and add it to the array
+  const addTracks = useCallback(
+    (track) => {
+      if (playlistTracks.some((savedTrack) => savedTrack.id === track.id))
+        return;
+      setPlaylistTracks((prevTrack) => [...prevTrack, track]);
+  }, [playlistTracks]);
+  // create a removeTrack that filters the tracks and removes it from the array
+  const removeTrack = useCallback((track) => {
+
+  }, []);
+  // create a update playlist name by using useCallback and setplaylistname
+  const updatePlaylist = useCallback(() => {
+
+  }, []);
+
+  // create a save playlist using useCallback and map them to spotify.js to save it back to the api servers.
+  const savePlaylist = useCallback(() => {
+
+  }, []);
+
+  return (
+  <div>
+    {/* need to setup html return */}
+  </div>
+);
 }
 
 export default App;
